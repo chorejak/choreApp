0e70232ee37b7cab1afa82ce305c3ee1732cd308
Alice Park <alicep@fb.com>
2018-07-13 11:46:52 -0700

    first commit

---
+++ Chore/ChoreTests/ChoreTests.m
@@ -0,0 +1,39 @@
+//
+//  ChoreTests.m
+//  ChoreTests
+//
+//  Created by Alice Park on 7/13/18.
+//  Copyright © 2018 Alice Park. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface ChoreTests : XCTestCase
+
+@end
+
+@implementation ChoreTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
